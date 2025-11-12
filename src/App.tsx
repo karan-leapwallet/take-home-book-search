006675cb882c1bf5b679eeef10156e9698e58db6
karan-coder <jivanikaran@gmail.com>
2025-11-12 17:52:12 +0530

    Add react-virtuoso for virtualization in book list; refactor useBooks hook to support multiple queries and improve loading/error handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useMemo, useRef, useState } from "react";
+import { Virtuoso } from "react-virtuoso";
 import { useBooks } from "./hooks/use-books";
 import { useDebouncedValue } from "./hooks/use-debounced-value";
 
@@ -9,31 +10,43 @@ function App() {
     useBooks(debouncedSearchQuery);
 
   const allBooks = useMemo(() => {
-    const _books = Object.values(books).flat();
+    const _books = Object.values(books[debouncedSearchQuery] || {}).flat();
     const lastElement: ("error" | "loader")[] = [];
-    if (error) {
+    if (error[debouncedSearchQuery]) {
       lastElement.push("error");
-    } else if (hasMore) {
+    } else if (hasMore[debouncedSearchQuery]) {
       lastElement.push("loader");
     }
     return [..._books, ...lastElement];
-  }, [books, hasMore, error]);
+  }, [
+    books[debouncedSearchQuery],
+    hasMore[debouncedSearchQuery],
+    error[debouncedSearchQuery],
+    debouncedSearchQuery,
+  ]);
 
+  console.log(books, allBooks);
   const ref = useRef<HTMLDivElement>(null);
 
   // alternative approach can be to use last element and if it is visible start fetching
   // I just used this approach as it came to mind first
-  const handleScroll = useCallback(
-    (data: React.UIEvent<HTMLDivElement>) => {
-      if (
-        data.currentTarget.scrollTop + data.currentTarget.clientHeight >=
-        data.currentTarget.scrollHeight - 100
-      ) {
-        fetchNextPage();
-      }
-    },
-    [fetchNextPage]
-  );
+  // const handleScroll = useCallback(
+  //   (data: React.UIEvent<HTMLDivElement>) => {
+  //     if (
+  //       data.currentTarget.scrollTop + data.currentTarget.clientHeight >=
+  //       data.currentTarget.scrollHeight - 100
+  //     ) {
+  //       fetchNextPage();
+  //     }
+  //   },
+  //   [fetchNextPage]
+  // );
+
+  const handleEndReached = useCallback(() => {
+    if (hasMore) {
+      fetchNextPage();
+    }
+  }, [fetchNextPage, hasMore]);
 
   return (
     <div className="bg-gray-100 h-screen w-screen flex flex-col gap-4">
@@ -44,11 +57,7 @@ function App() {
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
-      <div
-        className="flex-1 flex flex-col gap-4 overflow-y-auto"
-        ref={ref}
-        onScrollEndCapture={handleScroll}
-      >
+      <div className="flex-1 flex flex-col gap-4 overflow-y-auto">
         <div className="bg-white p-4 rounded-md">
           <h3 className="text-lg font-bold">Book Title</h3>
           <p className="text-gray-500">Author</p>
@@ -57,40 +66,51 @@ function App() {
         can add virtualization if library is allowed
         Also can improve the loading state and error state
         */}
-        {isLoading ? (
-          <div>Loading...</div>
+        {isLoading[debouncedSearchQuery] ? (
+          <div>Overall Loading...</div>
         ) : (
-          allBooks.map((book) => {
-            if (typeof book === "string") {
-              if (book === "loader") {
-                return (
-                  <div
-                    key={book}
-                    className="flex justify-center items-center p-5 h-10"
-                  >
-                    Loading...
-                  </div>
-                );
-              }
+          <>
+            <Virtuoso
+              data={allBooks}
+              className="flex-1"
+              endReached={handleEndReached}
+              itemContent={(index, book) => {
+                if (typeof book === "string") {
+                  if (book === "loader") {
+                    return (
+                      <div
+                        ref={ref}
+                        key={book}
+                        className="flex justify-center items-center p-5 h-10"
+                      >
+                        Loading...
+                      </div>
+                    );
+                  }
 
-              if (book === "error") {
+                  if (book === "error") {
+                    return (
+                      <div
+                        key={book}
+                        className="flex justify-center items-center p-5 h-10"
+                      >
+                        Error fetching books:{" "}
+                        {error[debouncedSearchQuery] ?? "Unknown error"}
+                      </div>
+                    );
+                  }
+                }
                 return (
-                  <div
-                    key={book}
-                    className="flex justify-center items-center p-5 h-10"
-                  >
-                    Error fetching books: {error ?? "Unknown error"}
+                  <div key={book.key}>
+                    <h3 className="text-lg font-bold">{book.title}</h3>
+                    <p className="text-gray-500">
+                      {book.author_name?.join(", ")}
+                    </p>
                   </div>
                 );
-              }
-            }
-            return (
-              <div key={book.key}>
-                <h3 className="text-lg font-bold">{book.title}</h3>
-                <p className="text-gray-500">{book.author_name?.join(", ")}</p>
-              </div>
-            );
-          })
+              }}
+            />
+          </>
         )}
       </div>
     </div>
--- src/hooks/use-books.ts
+++ src/hooks/use-books.ts
@@ -9,60 +9,66 @@ export type Book = {
 };
 
 export const useBooks = (query: string) => {
-  const [page, setPage] = useState(1);
-  const [books, setBooks] = useState<Record<number, Book[]>>({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [hasMore, setHasMore] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const fetchBooks = useCallback(
-    async (toReset: boolean = false) => {
-      try {
-        if (page === 1 || toReset) {
-          setIsLoading(true);
-          setBooks({});
-        }
-        setError(null);
-        const pageToFetch = toReset ? 1 : page;
-        let shouldRetry = true;
-        let res: Response | null = null;
-        let retryCount = 0;
-        while (shouldRetry) {
-          try {
-            res = await fetch(`${BASE_URL}?q=${query}&page=${pageToFetch}`);
-            shouldRetry = false;
-          } catch (error) {
-            retryCount++;
-            if (retryCount > 3) {
-              throw error;
-            }
-            await new Promise((resolve) => setTimeout(resolve, 1000));
-          }
-        }
-        if (!res) {
-          throw new Error("Error fetching books");
-        }
-        const data = await res.json();
-        setHasMore(data?.docs?.length > 0 && data?.num_found - data?.start > 0);
-        setBooks((prev) => ({ ...prev, [pageToFetch]: data?.docs ?? [] }));
-        setPage(pageToFetch + 1);
-      } catch (error) {
-        setError(
-          error instanceof Error ? error.message : "Error fetching books..."
-        );
-        // TODO: can add a retry mechanism here
-        // otherwise will have to think how do we want to handle error when paging latest page
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    [query, page]
+  const [page, setPage] = useState<Record<string, number>>({});
+  const [books, setBooks] = useState<Record<string, Record<number, Book[]>>>(
+    {}
   );
+  const [isLoading, setIsLoading] = useState<Record<string, boolean>>({});
+  const [hasMore, setHasMore] = useState<Record<string, boolean>>({});
+  const [error, setError] = useState<Record<string, string | null>>({});
+
+  const fetchBooks = useCallback(async () => {
+    try {
+      const pageToFetch = page[query] || 1;
+      if (pageToFetch === 1) {
+        setIsLoading((prev) => ({ ...prev, [query]: true }));
+      }
+      setError((prev) => ({ ...prev, [query]: null }));
+      let shouldRetry = true;
+      let res: Response | null = null;
+      let retryCount = 0;
+      while (shouldRetry) {
+        try {
+          res = await fetch(`${BASE_URL}?q=${query}&page=${pageToFetch}`);
+          shouldRetry = false;
+        } catch (error) {
+          retryCount++;
+          if (retryCount > 3) {
+            throw error;
+          }
+          await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+      }
+      if (!res) {
+        throw new Error("Error fetching books");
+      }
+      const data = await res.json();
+      setHasMore((prev) => ({
+        ...prev,
+        [query]: data?.docs?.length > 0 && data?.num_found - data?.start > 0,
+      }));
+      setBooks((prev) => ({
+        ...prev,
+        [query]: { ...(prev[query] || {}), [pageToFetch]: data?.docs ?? [] },
+      }));
+      setPage((prev) => ({ ...prev, [query]: pageToFetch + 1 }));
+    } catch (error) {
+      setError((prev) => ({
+        ...prev,
+        [query]:
+          error instanceof Error ? error.message : "Error fetching books...",
+      }));
+      // TODO: can add a retry mechanism here
+      // otherwise will have to think how do we want to handle error when paging latest page
+    } finally {
+      setIsLoading((prev) => ({ ...prev, [query]: false }));
+    }
+  }, [query, page]);
 
   useEffect(() => {
-    if (!query) return;
+    if (!query || !!books[query]) return;
 
-    fetchBooks(true);
+    fetchBooks();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [query]);
 
